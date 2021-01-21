6bf5bc24c4480cd2635c1406c837d585111ac369
VCI\guriqbal.singh <guriqbal.singh@visioncritical.com>
2021-01-21 13:20:46 -0800

    Reproduces error when optimiation.runtimeChunk is single

---
+++ app1/webpack.config.js
@@ -0,0 +1,53 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { ModuleFederationPlugin } = require("webpack").container;
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index",
+  mode: "development",
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    port: 5001,
+  },
+  output: {
+    publicPath: "auto",
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+    runtimeChunk: 'single',
+  },
+  module: {
+    rules: [
+      {
+        test: /bootstrap\.js$/,
+        loader: "bundle-loader",
+        options: {
+          lazy: true,
+        },
+      },
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: ["@babel/preset-react"],
+        },
+      },
+    ],
+  },
+  //http://localhost:3002/remoteEntry.js
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "app1",
+      remotes: {
+        app2: "app2@http://localhost:5002/remoteEntry.js",
+      },
+      shared: { react: { singleton: true }, "react-dom": { singleton: true } },
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
