90fda1353539ae8991a352eab65ea6278f39799b
VCI\guriqbal.singh <guriqbal.singh@visioncritical.com>
2021-01-25 17:43:35 -0800

    Respects chunk template

--- apps/app2/plugins/moduleFedSingleRuntime.js
+++ apps/app2/plugins/moduleFedSingleRuntime.js
@@ -1,21 +1,36 @@
-//Reference: https://github.com/module-federation/nextjs-mf
+/* eslint-disable consistent-return */
+/* eslint-disable no-underscore-dangle */
+// Reference: https://github.com/module-federation/nextjs-mf
 
+// eslint-disable-next-line import/no-extraneous-dependencies
 const ConcatSource = require('webpack-sources/lib/ConcatSource');
 
 module.exports = class ModuleFedSingleRuntimePlugin {
   constructor(options) {
-    this._options = {fileName: 'remoteEntry.js', ...options};
+    this._options = { fileName: 'remoteEntry.js', ...options };
   }
+
   // Define `apply` as its prototype method which is supplied with compiler as its argument
   apply(compiler) {
-    if (!this._options) return null;
-    const options = this._options;
-
     // Specify the event hook to attach to
-    compiler.hooks.emit.tap("EnableSingleRunTimeForFederationPlugin", (compilation) => {
-      const { assets } = compilation;
-      const runtime  = assets['runtime.js'];
-      const remoteEntry = assets[this._options.fileName];
+    compiler.hooks.emit.tap('EnableSingleRunTimeForFederationPlugin', (compilation) => {
+      const { assets, namedChunks } = compilation;
+      const runtimeChunk = namedChunks.get('runtime');
+      if (!runtimeChunk) {
+        throw new Error('No runtime single configured, so use of the plugin not required');
+      }
+      const runtime = assets[runtimeChunk.files[0]];
+
+      const remoteName = this._options.fileName;
+      if (!remoteName) {
+        throw new Error('No remote file configured, please pass fileName:{myRemote.js} in options');
+      }
+      const remoteEntry = assets[remoteName];
+      if (!remoteEntry) {
+        throw new Error(
+          `No remote entry ${remoteName} found, please make sure module federation configured properly else you dont need this plugin`
+        );
+      }
       const mergedSource = new ConcatSource(runtime, remoteEntry);
       assets[this._options.fileName] = mergedSource;
     });
